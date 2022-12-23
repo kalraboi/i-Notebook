faffb2ae178c03d2fe3954d3670cff93f337e6a2
kalraboi <118663295+kalraboi@users.noreply.github.com>
2022-12-23 17:49:11 +0530

    first commit

---
+++ Backend/routes/auth.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const bcrypt=require('bcryptjs')
+var jwt=require('jsonwebtoken');
+
+const JWT_SECRET="Kalra is a dashin boi";
+
+
+//Create a User using : POST "/api/auth/createUser". Doesn't require Auth
+router.post('/createuser',[
+    body('name','Enter a valid name').isLength({min:3}),
+    body('email','Enter a valid mail').isEmail(),
+    body('password','Must be atleast 5 characters').isLength({min:5}) 
+    ],
+    async (req,res)=>{
+        //If errors found, return bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+    //Check if user with this email already exists    
+
+    try{
+
+
+    let user= await User.findOne({email : req.body.email});
+    if(user){
+        return res.status(400).json({error:"User with this email already exists"})
+    }
+    const salt= await bcrypt.genSalt(10);
+    const secPass=await bcrypt.hash(req.body.password,salt);
+    //Create a new user
+      user = await User.create({
+      name: req.body.name,
+      password: secPass,
+      email: req.body.email
+    })
+    const data ={
+        user:{
+            id:user.id
+        }
+    }
+    const authtoken=jwt.sign(data,JWT_SECRET);
+    // res.json(user);
+    res.json(authtoken)
+} 
+catch(error)
+{
+    console.error(error.message);
+    res.status(500).send("Some error occured");
+}
+
+})
+
+module.exports=router
